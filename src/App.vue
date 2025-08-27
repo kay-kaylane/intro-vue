490bbd75b98c0f7d33a3ef92cc17ca47dbed9164
kay-kaylane <kaylane.miranda@aluno.senai.br>
2025-08-27 10:21:50 -0300

    exemplo11

--- src/App.vue
+++ src/App.vue
@@ -7,12 +7,19 @@
 // import Exemplo7 from './components/Exemplo07Clique.vue'
 // import Exercicio7 from './components/exercicios/Exercicio7.0.vue'
 // import Exemplo9 from './components/Exemplo09Enviar.vue'
-import UserCard from './components/Exemplo10Props.vue';
-import UserCard from './components/Exercicio9.0.vue';
+//import UserCard from './components/Exemplo10Props.vue';
+//import UserCard from './components/Exercicio9.0.vue';
+import { ref } from 'vue';
+import Exemplo11Emits from './components/Exemplo11Emits.vue';
+const totalCliques = ref(0)
+
+function atualizarTotal(valor){
+    totalCliques.value = valor
+}
+
 </script>
 
 <template>
-    <Exemplo9></Exemplo9>
-    <UserCard nome="Kaylane" idade="17" telefone="12343211" :ativo="True"></UserCard>
-    <ProductCard title="Smartphone" price="1999.99"  image="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR72Hz7MdPnzJyIoEiALo0PSP6DPUL01Ztzgw&s" :in-stock="True"></ProductCard>
+   <Exemplo11Emits @update="atualizarTotal"></Exemplo11Emits>
+   <p>Total de cliques: {{ totalCliques }}</p>
 </template>
---
+++ src/components/Exemplo11Emits.vue
@@ -0,0 +1,18 @@
+<script setup>
+    import { ref } from 'vue';
+    const emit = defineEmits(['update'])
+    const count = ref(0)
+
+    function incrementar(){
+        count.value++
+        emit('update', count.value)
+    }
+</script>
+
+<template>
+    <div>
+        <p>Contador:{{ count }}</p>
+        <button @click ='incrementar'>+1</button>
+        
+    </div>
+</template>
