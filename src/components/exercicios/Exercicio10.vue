5c2519bb4bf8f7e669c8da78e4bd1b6ac5a0d13c
kay-kaylane <kaylane.miranda@aluno.senai.br>
2025-09-10 07:19:43 -0300

    ex10

---
+++ src/components/exercicios/Exercicio10.vue
@@ -0,0 +1,16 @@
+<script setup>
+const props = defineProps({
+        name: String,
+        price: Number,
+    })
+</script>
+
+<template>
+    <div class="card" v-if="name">
+        <h3>{{ nome }}</h3>
+        <p v-if="email">Email: {{ email }}</p>
+        <p v-if="telefone">Telefone: {{ telefone }}</p>
+        <span v-if="idade">{{ idade }} anos</span>
+        <div :class="ativo ? 'ativo' : 'inativo'">{{ ativo ? 'Ativo' : 'Inativo' }}</div>
+    </div>
+</template>
